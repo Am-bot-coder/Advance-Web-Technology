8f988529e6ce2ad3f47563556b6ed79fd390822d
Ayush Mahadik <ayushmahadik49@gmail.com>
2026-09-18 16:08:42 +0530

    object creation using object template

---
+++ Java_Script/Object_creation/using_new_keyword/obj1.js
@@ -0,0 +1,14 @@
+//object has properties(variable) and method(functions)
+
+person1 = {}
+person1.name = 'Anil'
+person1["age"] = 63
+
+console.log(person1)
+
+person2 = {
+    name:"Ayush",
+    age:22
+}
+
+console.log(person2)
