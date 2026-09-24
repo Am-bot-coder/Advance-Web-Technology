7e16737a2566ea8eb9c1278680cb2b8d57e66008
Ayush Mahadik <ayushmahadik49@gmail.com>
2026-09-24 21:00:56 +0530

    GET with sql in users

--- Backend/server1/routes/user.js
+++ Backend/server1/routes/user.js
@@ -1,13 +1,17 @@
 const express = require('express')
+const pool = require('../utils/pool')
 route = express.Router()
 
-route.get('/user',(req,res)=>{
-    res.send("route get works")
+route.get('/user',async (req,res)=>{
+    sql = 'SELECT * FROM user;'
+    data = await pool.query(sql)
+    // res.send(data) --> It gave an array contain  the data + buffer
+    res.send(data[0])
 })
 
 route.post('/user',(req,res)=>{
     data = req.body
-    res.send(data)
+    sql = 'INSERT INTO USER '
 })
 
 
