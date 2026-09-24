36aa6dc5dd1bb3b2a6489259295fe29875c39997
Ayush Mahadik <ayushmahadik49@gmail.com>
2026-09-24 15:53:44 +0530

    note added

--- Backend/server1/server.js
+++ Backend/server1/server.js
@@ -3,7 +3,7 @@ port = 4000
 const userRoute = require('./routes/user')
 app = express()
 
-app.use(express.json())
+app.use(express.json()) // always need when you need to display or put data in form of object
 app.use(userRoute)
 
 
