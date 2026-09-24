e33cd49827a141db7dc254778ec7632259db2a08
Ayush Mahadik <ayushmahadik49@gmail.com>
2026-09-24 21:21:18 +0530

    put created

--- Backend/server1/routes/user.js
+++ Backend/server1/routes/user.js
@@ -12,7 +12,14 @@ route.get('/user',async (req,res)=>{
 route.post('/user',async(req,res)=>{
     const{name,mobile,email,pass} = req.body
     sql = 'INSERT INTO USER(name,email,mobile,pass) VALUES(?,?,?,?)'
-    data = await pool.query(sql,[name,mobile,email,pass])
+    data = await pool.query(sql,[name,email,mobile,pass])
+    res.send(data[0])
+})
+
+route.put("/user",async(req,res)=>{
+    const{uid,mobile} = req.body
+    sql = 'UPDATE user SET mobile = ? WHERE uid = ?'
+    data = await pool.query(sql,[mobile,uid])
     res.send(data[0])
 })
 
